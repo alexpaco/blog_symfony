785c8dd0d56e1a570145fb2cfdf0445a9d95de6f
alexpaco <alexandrepacoret@yahoo.fr>
2018-08-07 17:28:38 +0200

    Installation et configuration de FOSUserBundle

---
+++ src/Byme/BlogBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Byme\BlogBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
